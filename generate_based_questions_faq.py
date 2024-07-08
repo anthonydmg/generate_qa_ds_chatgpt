4440e9f8c7da7a06acff519edee919b1ec13cdd8
anthony <anthonymirandagil@gmail.com>
2024-07-08 17:05:17 -0500

    []

--- generate_based_questions_faq.py
+++ generate_based_questions_faq.py
@@ -28,7 +28,10 @@ def extract_faqs(text):
 ## esta bueno pero solo falta un poco
 # presenten una variedad de enfoques en la formulación de las preguntas. Asegúrate de que las nuevas preguntas
 #  Varía tanto la persona gramatical (primera persona y tercera persona) como el nivel de formalidad del lenguaje (formal, semiformal o informal)
-def get_prompt_gen_questions_based_faq(faqs, num_questions = 40):
+# Instrucción 2. Utiliza una amplia variedad de enfoques en la formulación de las preguntas, como preguntas condicionales, de inferencia, de comparación, de causa y efecto, de definición, etc.
+# Utiliza una amplia variedad de enfoques en la formulación de las preguntas, como preguntas condicionales, de inferencia, de comparación, de causa y efecto, de definición, etc.
+
+def get_prompt_gen_questions_based_faq_vff(faqs, num_questions = 40):
     questions_answer_list = list_json_to_txt(faqs)
     print()
     prompt = f"""
@@ -36,14 +39,49 @@ Utilizando las preguntas y respuestas proporcionadas, genera una lista de {num_q
 Instrucciones:
 
 Instrucción 1. Lee cuidadosamente las preguntas y respuestas proporcionadas para comprender completamente el contexto.
-Instrucción 2. Utiliza una amplia variedad de enfoques en la formulación de las preguntas, como preguntas condicionales, de inferencia, de comparación, de causa y efecto, de definición, etc.
-Instrucción 3. Asegúrate que las preguntas generadas aborden tanto aspectos generales o amplios como detalles específicos, además que cada pregunta sea única y asegúrate que pueda ser respondida empleando empleando unicamente la información proporcionada en las respuestas de las preguntas proporcionadas.
-Instrucción 4. Presenta las {num_questions} preguntas generadas de la siguiente manera:
+Instrucción 2. Genera preguntas utilizando una amplia variedad de enfoques en la formulación de las preguntas, como preguntas condicionales, de inferencia, de comparación, de causa y efecto, de definición, etc, que aborden tanto aspectos generales o amplios como detalles específicos.  Asegurate que cada pregunta sea única y pueda ser respondida empleando unicamente la información en las respuestas de las preguntas proporcionadas entre tres comillas invertidas. 
+Instrucción 3. Presenta las {num_questions} preguntas generadas de la siguiente manera:
 1. [Aquí va la primera pregunta]
 2. [Aquí va la segunda pregunta]
 ...
-Preguntas y Respuestas:
-{questions_answer_list}
+Preguntas y Respuestas:```
+{questions_answer_list}```
+"""
+    return prompt
+
+def get_prompt_2_gen_questions_based_faq(faqs, num_questions = 40):
+    questions_answer_list = list_json_to_txt(faqs)
+    print()
+    prompt = f"""
+Utilizando las preguntas y respuestas proporcionadas, genera una lista de {num_questions} preguntas únicas que abarquen diferentes niveles de complejidad y cubran tanto puntos generales o amplios hasta detalles específicos. Cada pregunta debe tener una respuesta dentro de la información proporcionada. Sigue estas instrucciones:
+Instrucciones:
+
+Instrucción 1. Lee cuidadosamente las preguntas y respuestas proporcionadas para comprender completamente el contexto.
+Instrucción 2. Genera utilzando una amplia variedad de enfoques en la formulación de las preguntas, como preguntas condicionales, de inferencia, de comparación, de causa y efecto, de definición, etc. y asegurandote que cada pregunta sea única y pueda ser respondida empleando unicamente la información en las respuestas de las preguntas proporcionadas entre tres comillas invertidas. 
+Instrucción 3. Presenta las {num_questions} preguntas generadas de la siguiente manera:
+1. [Aquí va la primera pregunta]
+2. [Aquí va la segunda pregunta]
+...
+Preguntas y Respuestas:```
+{questions_answer_list}```
+"""
+    return prompt
+# abarquen diferentes niveles de complejidad y 
+def get_prompt_1_gen_questions_based_faq(faqs, num_questions = 40):
+    questions_answer_list = list_json_to_txt(faqs)
+    print()
+    prompt = f"""
+Utilizando las preguntas y respuestas proporcionadas, genera una lista de {num_questions} preguntas únicas que cubran tanto puntos generales o amplios hasta detalles específicos. Cada pregunta debe tener una respuesta dentro de la información proporcionada. Sigue estas instrucciones:
+Instrucciones:
+
+Instrucción 1. Lee cuidadosamente las preguntas y respuestas proporcionadas para comprender completamente el contexto.
+Instrucción 2. Genera preguntas que aborden tanto aspectos generales o amplios como detalles específicos, asegurandote que cada pregunta sea única y pueda ser respondida empleando unicamente la información en las respuestas de las preguntas proporcionadas entre tres comillas invertidas. 
+Instrucción 3. Presenta las {num_questions} preguntas generadas de la siguiente manera:
+1. [Aquí va la primera pregunta]
+2. [Aquí va la segunda pregunta]
+...
+Preguntas y Respuestas:```
+{questions_answer_list}```
 """
     return prompt
 
@@ -84,7 +122,7 @@ def generate_questions_based_faq(faqs):
     num_questions = get_num_questions(total_tokens)
     
     print("num_questions:", num_questions)
-    prompt = get_prompt_gen_questions_based_faq(list_faqs, num_questions)
+    prompt = get_prompt_1_gen_questions_based_faq(list_faqs, num_questions)
 
     messages =  [{'role':'user', 'content': prompt}]
 
@@ -94,8 +132,25 @@ def generate_questions_based_faq(faqs):
                         #model="gpt-3.5-turbo-0613",
                         temperature=0)
 
-    print("response:",response)
-    preguntas = extract_questions_from_response_regex(response)
+    print("response prompt-1:",response)
+
+    preguntas_prompt_1 = extract_questions_from_response_regex(response)
+
+    time.sleep(5)
+    prompt = get_prompt_2_gen_questions_based_faq(list_faqs, num_questions)
+
+    messages =  [{'role':'user', 'content': prompt}]
+
+    response = get_completion_from_messages(
+                        messages,
+                        model = "gpt-4o-2024-05-13",
+                        #model="gpt-3.5-turbo-0613",
+                        temperature=0)
+
+    print("\nresponse prompt-2:",response)
+    preguntas_prompt_2 = extract_questions_from_response_regex(response)
+    
+    preguntas = preguntas_prompt_1 + preguntas_prompt_2
     return preguntas
     #print("Preguntas:", preguntas)
 
@@ -132,7 +187,7 @@ for iter in range(times_samples):
         group_faqs = [faqs[id] for id in group_ids]
         preguntas = generate_questions_based_faq(group_faqs)
         time.sleep(4)
-
+        break
         additional_faqs.append({
             "original_questions":  [ faq["question"] for faq in group_faqs], 
             "additional_questions": preguntas
