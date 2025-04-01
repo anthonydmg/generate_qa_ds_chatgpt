b7ebd5ea44ca2cb8408e02cd70859631f301fc6a
anthony <anthonymirandagil@gmail.com>
2025-03-31 19:34:07 -0500

    []

--- test_identi_contextual_questionV2.py
+++ test_identi_contextual_questionV2.py
@@ -1507,7 +1507,7 @@ El objetivo es que el asistente pueda comprender y responder adecuadamente la pr
 🔎 Paso 1: Análisis del ultimo mensaje del usuario
 Examina el último mensaje del usuario y analiza los siguientes aspectos:
 A. Reconocimiento de consulta
-Determina el usuario esta realizando una consulta en su ultimo mensaje. En caso no realize alguna consulta en el resto de los pasos responde con "No aplica".
+Determina si el usuario esta realizando una consulta en su ultimo mensaje. En caso no realize alguna consulta en el resto de los pasos responde con "No aplica".
 
 B. Identificación del tema de consulta
 Identifica de que trata la consulta del usuario y describelo de manera concisa.
@@ -1603,6 +1603,101 @@ Datos de Entrada
 Historial previo de la conversación: <<{history_chat}>>"""
         return prompt_identify_reform
 
+
+def get_prompt_reformulated_contextual_query_21(query, history_chat_messages):
+        history_chat = format_text_history_chat(history_chat_messages)
+        prompt_identify_reform = f"""Dado el último mensaje del usuario, dirigido a un asistente especializado en normativas académicas de la Facultad de Ciencias de la Universidad Nacional de Ingeniería (UNI), analiza el historial previo de la conversación junto con la consulta del usuario en su último mensaje y determina si es necesario reformularla para mejorar su precisión y claridad.
+
+El objetivo es que el asistente pueda comprender y responder adecuadamente la pregunta del usuario sin tener acceso al historial de la conversación.
+
+🔎 Paso 1: Análisis del ultimo mensaje del usuario
+Examina el último mensaje del usuario y analiza los siguientes aspectos:
+A. Reconocimiento de consulta
+Determina si el usuario esta realizando una consulta en su ultimo mensaje. En caso no realize alguna consulta en el resto de los pasos responde con "No aplica".
+
+🔎 Paso 2: Evaluación de claridad del mensaje sin el historial previo 
+Alineado con la información que esta expliciamente descrita en el ultimo mensaje, evalua si un asistente puede responder de manera adecuada y precisa a la consulta del usario sin necesidad del historial previo. Se detallado con tu explicacion.
+
+🔎 Paso 3: Evaluación del historial para mejorar la pregunta
+Verifica si el historial previo de la conversación contiene información relevante que el usuario no haya mencionado explícitamente en su último mensaje, pero que pueda mejorar la claridad y precisión de la consulta. El objetivo es incorporar estos términos o detalles específicos en la reformulación del mensaje para que la consulta sea más clara y pueda ser respondida con precisión, sin depender del historial previo.
+Presta especial atención a términos especificos que no hayan sido mencionados explícitamente en el ultimo mensaje del usuario, pero que sí aparecieron en mensajes anteriores y sean esenciales para la consulta, como tipos de constancias, solicitudes o carnets. Ejemplo: en el ultimo mensaje no se menciona que se refiere al carnert universitario si no solo menciona "carnet" lo cual es relevante para que el asistente sin acceso al historial de la conversación responde de manera precisa.
+Si el historial previo de la conversación no aporta datos útiles para mejorar la consulta, indícalo sin hacer suposiciones ni sugerencias adicionales. Enfócate solo en si el historial previo de la conversación contiene información que ayudaría a mejorar la pregunta.
+
+✅ Paso 4: Decisión sobre la reformulación
+Antes de tomar una decisión, proporciona una explicación justificando si es o no necesario reformular la pregunta, base en los siguientes criterios:
+ - Ambigüedad sin el historial: Si la consulta en el último mensaje es completamente ambigua y el historial contiene información relevante para hacerla más clara, entonces la pregunta debe ser reformulada.
+ - Precisión del tema: Si el historial contiene detalles que no han sido mencionados explícitamente en el último mensaje y que pueden hacer la pregunta más precisa y facilitar una mejor respuesta, entonces la pregunta debe ser reformulada. El objetivo es que el asistente pueda comprender y responder adecuadamente la pregunta del usuario sin tener acceso al historial de la conversación.
+ - Ámbito institucional implícito: Todas las consultas se asumen relacionadas con esta facultad. No reformules la pregunta únicamente para incluir “Facultad de Ciencias de la UNI” a menos que la omisión de esta referencia haga que la pregunta sea completamente ambigua.
+
+Formato de Respuesta Esperado
+El último mensaje contiene una pregunta: Sí/No
+Determina si es necesario reformular la consulta con los criterios mencionados anteriormente y responde utilizando el siguiente formato:
+Evaluación de la claridad: [Análisis detallado si el ultimo mensaje del usuario en lo suficiente claro para que un asistente pueda responder de manera adecuada y precisa a la consulta del usuario sin necesidad del historial previo].
+Evaluación del historial para mejorar la pregunta:  [Explicacion sobre si el historial previo de la conversación contiene información relevante que el usuario no haya mencionado explícitamente en su último mensaje y que podría ayudar a mejorar la claridad y precisión de la consulta]
+Análisis: [Explicación detallada sobre por qué es o no estrictamente necesario reformular la pregunta].
+Es estrictamente necesario reformular la consulta: Sí/No/No aplica  
+
+Reformulación: <<Pregunta reformulada/No aplica>>
+
+Ejemplos de aplicacion de criterios:
+
+Ejemplo 1 (Reformulacion es necesaria)
+
+Último mensaje del usuario: ¿Sabes si hay algún plazo específico que deba tener en cuenta para la orden de pago?
+
+Historial previo de la conversación: <<
+user: ¿Cuáles son las implicaciones de no generar la orden de pago antes de realizar el pago del autoseguro?
+assistant: Si no se genera la orden de pago antes de realizar el pago del autoseguro, el estudiante podría enfrentar problemas para matricularse de manera regular. Es fundamental cumplir con el plazo máximo establecido en el calendario de actividades académicas, ya que de no hacerlo, la matrícula podría no habilitarse y el estudiante tendría que gestionar su matrícula como rezagado. Para verificar la situación de la matrícula, se recomienda comprobar si se habilita en las fechas de matrícula regular. Si no es así, es necesario comunicarse con la oficina de estadística (AERA) para obtener más información.>>
+
+Análisis: El último mensaje del usuario aborda la preocupación sobre los plazos para generar la orden de pago. La pregunta sin el historial es clara, sin embargo, la ambigüedad radica en que no se especifica si se refiere a la orden de pago en general o a un contexto particular. En el historial previo de la conversación se puede deducir que la orden de pago es para el autoseguro, este detalle es relevante para hacer la pregunta mas clara y precisa, por lo tanto, sería útil reformular la pregunta para que incluya el contexto del autoseguro y asistente pueda comprender y responder adecuadamente la pregunta del usuario sin tener acceso al historial de la conversación. 
+
+El último mensaje contiene una pregunta: Sí  
+
+Es estrictamente necesario reformular la consulta: Sí  
+
+Reformulación: ¿Cuál es el plazo específico que debo tener en cuenta para generar la orden de pago del autoseguro, y qué pasos debo seguir si no logro hacerlo a tiempo y tengo que matricularme como rezagado?
+
+
+Ejemplo 2 (Reformulacion no es necesaria)
+
+Último mensaje del usuario: ¿Cómo puedo hacer la solicitud de la constancia de notas? ¿Y caunto tiempo tarda el tramite?
+
+Historial previo de la conversación: <<
+user: ¿Qué entidad envía la orden de pago al estudiante después de que este solicita la constancia de notas?
+assistant: La orden de pago es enviada al estudiante por la oficina de estadística (AERA) después de que este solicita la constancia de notas.>>
+
+Análisis: La pregunta del usuario en su último mensaje se centra en el proceso para solicitar una constancia de notas y el tiempo de demora del proceso, lo cual es un tema específico y claro. El historial previo de la conversación no incluye información relevante que ayuda mejorar la precision o claridad de la pregunta ya que ya es lo suficiente clara y especifica con el que trata "proceso para solicitar una constancia de notas". Ademas, no es necesario incluir la relación con la facultad de ciencias de manera explicita, ya que no es estrictamente necesario. Por lo tanto, no es necesario reformular la consulta.
+
+El último mensaje contiene una pregunta: Sí  
+
+Es estrictamente necesario reformular la consulta: No  
+
+Reformulación: No aplica
+
+Ejemplo 3 (Mensaje No es una Pregunta)
+
+Último mensaje del usuario: "Gracias por la ayuda."
+
+Historial previo de la conversación: <<
+user: ¿Cuanto se puede solicitar el retiro parcial?
+assistant: Hasta la quinta semana de clases.
+
+Análisis: El ultimo mensaje del usuario no incluye una consulta por lo que la reformulación no aplica"
+El último mensaje contiene una pregunta: Sí  
+
+Es estrictamente necesario reformular la consulta: No aplica  
+
+Reformulación: No aplica
+
+
+Datos de Entrada
+
+Último mensaje del usuario: {query}
+
+Historial previo de la conversación: <<{history_chat}>>"""
+        return prompt_identify_reform
+
+
 ## Agregar esto a eso
 # La pregunta del usuario se refiere al proceso de matrícula en la universidad y si hay plazos específicos que deben considerarse. Aunque la pregunta es clara y directa, el contexto sobre qué tipo de matrícula se está refiriendo (por ejemplo, matrícula inicial, matrícula para un ciclo académico específico, etc.) no se menciona. Sin embargo, dado que el término "matrícula" es común en el ámbito académico y el asistente está familiarizado con las normativas de la universidad, se puede inferir que se refiere al proceso general de matrícula en la Facultad de Ciencias de la UNI. La pregunta es específica en cuanto a la búsqueda de información sobre el proceso y los plazos, lo que permite que se pueda responder de manera adecuada. Por lo tanto, hay suficiente contexto para entender la pregunta sin necesidad de información adicional.
 
@@ -1619,7 +1714,7 @@ count_good_pred = 0
 #test_data = train_contextualize_questions_not_need_context[150:160] + train_contextualize_questions_not_need_context[200:210]
 #save_json("./test/", "not_need_reformulate_demo_test_data_2", test_data)
 # 11, 12
-test_data = load_json("./test/not_need_reformulate_demo_test_data.json")[12:13]
+test_data = load_json("./test/not_need_reformulate_demo_test_data.json")[11:12]
 print("\nlen(test_data):", len(test_data))
 print()
 
